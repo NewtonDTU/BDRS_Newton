73ee0bebcda83171a55a43c197b463863053c498
martinszilagyi <szilagyimartin13@gmail.com>
2026-02-27 17:27:20 +0100

    added random route

--- robobot/mqtt_python/sedge.py
+++ robobot/mqtt_python/sedge.py
@@ -69,8 +69,8 @@ class SEdge:
     lineCtrl = False # private
     # try with a P-Lead controller
     lineKp = 0.9 # proportional gain (rad/s per sensor unit)
-    lineTauZ = 0.35 # derivative lead time (second)
-    lineTauP = 0 # integral lag time (second)
+    lineTauZ = 0.38 # derivative lead time (second)
+    lineTauP = 0.10 # integral lag time (second)
     # Lead pre-calculated factors
     tauP2pT = 1.0
     tauP2mT = 0.0
