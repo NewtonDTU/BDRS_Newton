1e9e4e33e6e5327a04e782b0079598ca5c270bc0
martinszilagyi <szilagyimartin13@gmail.com>
2026-02-27 17:43:39 +0100

    added random route

--- robobot/mqtt_python/sedge.py
+++ robobot/mqtt_python/sedge.py
@@ -68,7 +68,7 @@ class SEdge:
     # follow line controller
     lineCtrl = False # private
     # try with a P-Lead controller
-    lineKp   = 2.0
+    lineKp   = 1.3
     lineTauZ = 0.15
     lineTauP = 0.075
     # Lead pre-calculated factors
